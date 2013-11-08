c48249f4c25274c91e0bac16c9c9875381422701
vngs <hoangcaothang@gmail.com>
2013-11-08 18:08:18 +1345

    Updated to 1.1.1

---
+++ CNVrd2/R/AllGenerics.R
@@ -0,0 +1,19 @@
+setGeneric("countReadInWindow",
+           function(Object, ...){standardGeneric("countReadInWindow")})
+setGeneric("segmentSamples",
+           function(Object, ...){standardGeneric("segmentSamples")})
+setGeneric("segmentSamplesUsingPopInformation",
+           function(Object, ...){standardGeneric("segmentSamplesUsingPopInformation")})
+setGeneric("plotCNVrd2",
+           function(Object, ...){standardGeneric("plotCNVrd2")})
+setGeneric("emnormalCNV",
+           function(Object, ...){standardGeneric("emnormalCNV")})
+setGeneric("searchGroupCNVs",
+           function(Object, ...){standardGeneric("searchGroupCNVs")})
+setGeneric("groupCNVs",
+           function(Object, ...){standardGeneric("groupCNVs")})
+setGeneric("plotPolymorphicRegion",
+           function(Object, ...){standardGeneric("plotPolymorphicRegion")})
+setGeneric("identifyPolymorphicRegion",
+           function(Object, ...){standardGeneric("identifyPolymorphicRegion")})
+
